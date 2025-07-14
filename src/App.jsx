e5fb67fbe8dfdea36860ee63f09e12b84b2f53c5
Yoanly Perez <98776473+yoanlypa@users.noreply.github.com>
2025-07-14 19:31:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Header from './components/Header.jsx';
 import Login from "./pages/Login";
 import PedidosList from "./pages/PedidosList";
+import Cruceros from "./pages/Cruceros";
 import MisPedidos from "./pages/MisPedidos";
 import "./App.css";
 
@@ -32,6 +33,7 @@ export default function App() {
             <Route path="/" element={ <PrivateRoute> <PedidosList /> </PrivateRoute>} />
             <Route path="/" element={<Navigate to="/mis-pedidos" />} />
             <Route path="/mis-pedidos" element={<MisPedidos />} />
+            <Route path="/cruceros"   element={<Cruceros />} />
           </Routes>
         </QueryClientProvider>
       </AuthProvider>
